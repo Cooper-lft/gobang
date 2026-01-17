3cff5095a2de30a595f11ad24fd78df33863b9fd
LFT <2879977217@qq.com>
2026-01-17 17:39:28 +0800

    新增了工作模式选择:PVP/PVE

--- gobang.c
+++ gobang.c
@@ -1,8 +1,11 @@
+#include "include/myhead.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 15
 #define CHARSIZE 3
+#define PVP 1  //人人对战模式
+#define PVE 0  //人机对战模式
 void initRecordBorard(void);
 void innerLayoutToDisplayArray(void);
 void displayBoard(void);
@@ -41,16 +44,16 @@ char play2CurrentPic[]="△";
 int arrayForInnerBoardLayout[SIZE][SIZE];
 
 int main()
-
 {
-
-    initRecordBorard();    //初始化一个空棋盘
+    int workstate;
+    workstate=selectmode();
+    
 
     arrayForInnerBoardLayout[0][0]=1;    //在棋盘的左上角落一个黑色棋子
     innerLayoutToDisplayArray();  //将心中的棋盘转成用于显示的棋盘
     displayBoard();          //显示棋盘
     getchar();   
-
+/*
     arrayForInnerBoardLayout[5][9]=2; //innerboard的数组元素为1表示黑色棋子，2表示白色棋子，0表示没有棋子
     innerLayoutToDisplayArray();
     displayBoard();
@@ -70,6 +73,8 @@ int main()
     innerLayoutToDisplayArray();
     displayBoard();
     getchar();
+*/
+ 
     return 0;
 }
 
@@ -95,7 +100,7 @@ void innerLayoutToDisplayArray(void){
 
 
 	//第二步：扫描arrayForInnerBoardLayout，当遇到非0的元素，将●或者◎复制到arrayForDisplayBoard的相应位置上
-	//注意：arrayForDisplayBoard所记录的字符是中文字符，每个字符占2个字节。●和◎也是中文字符，每个也占2个字节。
+	//注意：arrayForDisplayBoard所记录的字符是中文字符，每个字符占3个字节(?)。●和◎也是中文字符，每个也占3个字节。
     for(int i=0;i<SIZE;i++){
         for(int j=0;j<SIZE;j++){
             if(arrayForInnerBoardLayout[i][j]!=0){
@@ -127,10 +132,10 @@ void displayBoard(void){
 	//第一步：清屏
 	system("clear");   //清屏  
 	//第二步：将arrayForDisplayBoard输出到屏幕上
-    for(int i=SIZE;i>0;i--){
+    for(int i=SIZE,k=0;i>0;i--,k++){
         printf("%2d ",i);
         for(int j=0;j<SIZE;j++){
-            printf("%c%c%c ",arrayForDisplayBoard[i][CHARSIZE*j],arrayForDisplayBoard[i][CHARSIZE*j+1],arrayForDisplayBoard[i][CHARSIZE*j+2]);
+            printf("%c%c%c ",arrayForDisplayBoard[k][CHARSIZE*j],arrayForDisplayBoard[k][CHARSIZE*j+1],arrayForDisplayBoard[k][CHARSIZE*j+2]);
         }
         printf("\n");
     }
@@ -143,5 +148,33 @@ void displayBoard(void){
 
     }
     printf("\n");
+
+    //显示作者名字
+    printf("作者：李丰廷\n");
 } 
 
+int selectmode(){
+
+    int workmode;
+    //选择下棋模式
+    while(1){
+        initRecordBorard();    //初始化一个空棋盘
+        innerLayoutToDisplayArray();
+        displayBoard();//显示棋盘
+        printf("请选择模式：人人对战(请输入1)/人机对战(请输入0)\n");
+        //读取并验证输入是否正确
+        if(scanf("%d",&workmode)==1 && (workmode==0||workmode==1) ){
+            break;
+        }else{
+            getchar();//吸收错误输入的空格
+            int c;
+            while((c=getchar())!='\n'&&c!=EOF){
+                ;
+            }
+            printf("\n输入错误,请按任意建后重新输入\n");
+            getchar();
+        }
+    }
+
+    return workmode;
+}
